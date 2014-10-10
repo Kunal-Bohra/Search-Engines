ac794795b2342d140d1e3fd79ce65fc915c93d95
Kunal <bohrak@ccs.neu.edu>
2014-10-10 02:01:47 -0400

    Updates the most recent solution
    Recursive is not that efficient.

--- Focused Crawling on Wiki/Crawler.py
+++ Focused Crawling on Wiki/Crawler.py
@@ -1,4 +1,5 @@
-#########  CS600 HW1: A Focused Web Crawler  ###########
+#########  A Focused Web Crawler  ###########
+
 
 '''
 Author: Kunal Bohra
@@ -16,26 +17,27 @@ import time
 import sys
 import re
 
-#Set recursion limit on the crawler.
-sys.setrecursionlimit(2500)
+#Constant Declaration
 
 #unseen_list is a List of URLs that are to be visited
 unseen_list = []
+
 #seen_list is a list of URLs that are visited
 seen_list = []
-#craw_depth is the halting measure of the crawling algorithm
-craw_depth = 3
-#is_keyphrase_present is a boolean to swtich between focused and unfocused crawling
-is_keyphrase_present = False
-#Sanity Check
-is_input_url_present = False
 
-#Final Crawled_List
+#List of all crawled urls
 result_list = []
+
+#This list contains urls with keyword
+new_list = []
+
+#It should be noted that in case of empty keyphrase, new_list and seen_list
+#has same entries.
+
 #Base depth to start crawling
 start_depth = 1
 
-#Constant fot crawling depth 
+#Constant is the halting measure of the crawling algorithm
 halt_limit = 4
 
 #Constant for Main Wiki Page
@@ -44,76 +46,132 @@ main_page = 'http://en.wikipedia.org/wiki/Main_Page'
 #Constant for base englisht wiki page
 base_page = 'http://en.wikipedia.org/wiki/'
 
-#Input Seed URL. Taken as command line argument
-baseURL = sys.argv[1]
-if sys.argv[1] != None:
-    #This boolean helps in switching mode later on
-    is_input_url_present = True
+#keyphrase = "information retrieval"
     
+#baseURL = "http://en.wikipedia.org/wiki/Gerard_Salton"
 
-#Check if optional second argument is present or not.
+#Set recursion limit on the crawler.
+sys.setrecursionlimit(2500)
+
+#Sanity Checks
+
 if len(sys.argv) < 2:
-    #This boolean helps in switching mode later on
-    is_keyphrase_present = False
+#This boolean helps in switching mode later on
+    print "Cannot start without Seed URL !!!!"
 else:
-    is_keyphrase_present = False
-    
-if is_keyphrase_present:
+    baseURL = sys.argv[1]
+
+if len(sys.argv) > 2:
+#There is a keyphrase as an optional keyword
     keyphrase = sys.argv[2]
+    print "Keyphrase is:  "+str(keyphrase)
 else:
-    keyphrase = ""
+    keyphrase=""
+
+
     
-    
-#Function to check for Canonical URLS
+#Function to check for Canonical URL
 def check_canonical(url):
     #Request page
-    print "In canonical function|n"
-    print url
+    
+    #Respect robots.txt Delay for 1 second
+    time.sleep(1)	
     request = urllib2.urlopen(url)
     
     #Read the page contents and store them
     response = request.read()
-    
+    request.close()
     #Parse the contents of web page
     soup = BeautifulSoup(response)
     
     #Final all link tages with canonical attribute
-    #links = soup.findAll('link', attrs={"rel":"canonical"})
     links = soup.findAll('link')
     
     #Check if we are inputted with a canonical URL
     for link in links:
         if "canonical"  in str(link.get('rel')):
-            #print link
-            print "Canonical is....."
-            print link.get('href')
+	    #print "In cannonical"
             return link.get('href')
     return url
-    
 
+#Function to validate a URL according to problem statement    
+def validate_url(url,seen_list,unseen_list):
+    if '#' in url:
+        updated_url = url.partition('#')[0]
+    else:
+        updated_url = url
+                
+    #URL should not be same as Main wiki page
+    constraintOne = updated_url != main_page
+            
+    #URL should not start with English wiki articles
+    constraintTwo = updated_url.startswith(base_page)
+                
+    #URL should not already be visited. Else we have cycles
+    constraintThree = updated_url not in seen_list
+                
+    #URL should be in unseen_list
+    constraintFour = updated_url not in unseen_list
+                
+    if constraintOne and constraintTwo and constraintThree and constraintFour:
+        return True
+    else:
+        return False
+
+#Function to scrap all text from a page.
+def scrap_page(soup):
+    #Scrap text from web page for focus crawling mode
+
+    #Remove all style and javascript elements
+    for script in soup(["style","script"]):
+        script.extract()
+        
+    #Fetch all text from page
+    text = soup.get_text()
+    
+    #Format the data 
+    text_line = (line.strip() for line in text.splitlines())
+    
+    #Create text chunks
+    blocks = (para.strip() for line in text_line for para in line.split("  "))
+    
+    #Final formatting
+    text = '\n'.join(block for block in blocks if block)
+
+    #Encode data to readable form
+    base_scrap = (text.encode('utf-8'))
+    return base_scrap
+
+    
 #Main crawler function. Implements a Breadth First Search but with limited depth over the 
 #search space.
-def crawl(result_list,unseen_list,seen_list,crawl_depth,seed_url,increaseDepth, depthCountdown):
+def crawl(result_list,unseen_list,seen_list,crawl_depth,seed_url,increaseDepth, depthCountdown, new_list):
     
     #Halt at given depth
     if crawl_depth == halt_limit:
+        print "Halting condition reached....."
+        print "Halting at depth :"+str(halt_limit)
         
         #Get final statistics 
-        result_list = seen_list+unseen_list
+        result_list = list(set(seen_list+unseen_list))
+        
         #Total number of crawled links
         links_crawled = len(result_list)
-        #print result_list
-        #print links_crawled
-        
+        matched_links = len(list(new_list))
+
         #Create an output file
         result_file = open("Crawled_Links.txt","w")
-        result_file.write("----------------Summary of Crawled "+str(links_crawled)+" Links"+"----------------"+"\n")
+        result_file.write("-----------   Summary of All Crawled "+str(links_crawled)+" Links"+"-----------"+"\n")
         for link in range(len(result_list)):
-            #print result_list[link]
             result_file.write(str(link+1)+"  "+result_list[link]+"\n")
+
+        #Create an output file
+        result_file = open("Focus_Crawl.txt","w")
+        result_file.write("----------    Summary of Links with Keyphrase "+str(links_crawled)+" Links"+"----------"+"\n")
+        for link in range(len(new_list)):
+            result_file.write(str(link+1)+"  "+new_list[link]+"\n")
             
         return 
-    
     
     
     #Condition variable to control depth movement
@@ -122,124 +180,102 @@ def crawl(result_list,unseen_list,seen_list,crawl_depth,seed_url,increaseDepth, 
         increaseDepth = False
 
     #Respect robots.txt Delay for 1 second
-    #time.sleep(1)
+    time.sleep(1)
     
     #Request the page to explore
     response = urllib2.urlopen(seed_url)
-    #print "Fetching URL...."+final_url
+    print "Processing URL....   "+seed_url
+
     page = response.read()
     response.close()
     soup = BeautifulSoup(page)
-   
-    #Scrap text from web page for focus crawling mode
-    base = soup.find_all(text = re.compile("\\b"+keyphrase+"\\b",re.IGNORECASE))        
+    #Page has been explored, add it to seen list
+    seen_list.append(seed_url)
+    
 
+    #Function to scrap text off the web page
+    base_scrap = scrap_page(soup)
+    
     #Flag to see if we have a match in base text in focused crawling mode
     flag = False
-    for i in range(len(base)):
-        if re.search(keyphrase, base[i], re.IGNORECASE) is not None:
-            flag = True
-            break
-        else:
-            flag = False
-                        
-    #If true, we have a match during focused crawling.
-    if flag:
-        #Add URL to seen list to avoid cycles.
-        seen_list.append(seed_url)    
+
+    #Generate the keyphrase to be matched
+    text_to_search = re.compile("\\b"+keyphrase+"\\b",re.IGNORECASE)
+
+
+    #Search for the keyword only if keyphrase is not an empty string
+    #Thus below check is skipped for unfocussed crawl.
+
+    
+    if re.search(text_to_search, base_scrap) != None:   
+        #If true, we have a match during focused crawling.
+        #print "Keyword Found"
         
-        #Explore the children of current URL
+        #This list has all urls that has keyword
+        new_list.append(seed_url)
+        flag = True
+    else:
+        #print"\n"
+        #print "The current "+seed_url+" doesn't have a keyword !!"
+        flag = False                        
+
+    if flag:
+    #We are in focus crawling    
+    #Explore the children of current URL  
         for link in soup.find_all('a'):
+            #print "childs are"
+            #print len(soup.find_all('a'))
             new_link = link.get('href')
+            #Construct child URL
             fullurl = urlparse.urljoin(seed_url, new_link)
-            
-            
                    
             #Condition check as given in the problem statement:
-            
-            if '#' not in fullurl:
-                
-                #URL should not be same as Main wiki page
-                constraintOne = fullurl != main_page
-                
-                #URL should not start with English wiki articles
-                constraintTwo = fullurl.startswith(base_page)
-                
-                #URL should not already be visited. Else we have cycles
-                constraintThree = fullurl not in seen_list
-                
-                #URL should be in unseen_list
-                constraintFour = fullurl not in unseen_list
-                
-                if constraintOne and constraintTwo and constraintThree and constraintFour and ':' not in new_link:
-                    sec_url =  check_canonical(fullurl)            
-		    if str(fullurl) != str(sec_url):
-		    #final_url = sec_url
-		    #else:
-		        print sec_url
-		        print fullurl
-		        fullurl = sec_url
-			
-		if fullurl not in seen_list and fullurl not in unseen_list:
-		    unseen_list.append(fullurl)
-		
-            
-            else:
-                main_Url = fullurl.partition('#')[0]
-                #print main_Url
-                #URL should not be same as Main wiki page    
-                constraintOneHashLink = main_Url != main_page
-                
-                #URL should not start with English wiki artices
-                constraintTwoHashLink = main_Url.startswith(base_page)
-                
-                #URL should not already be visited. Else we have cycles
-                constraintThreeHashLink = main_Url not in seen_list
-                
-                #URL should be in unseen_list
-                constraintFourHashLink = main_Url not in unseen_list
-                   
-                if constraintOneHashLink and constraintTwoHashLink and constraintThreeHashLink and constraintFourHashLink and ':' not in new_link: 
-		    sec_url =  check_canonical(main_Url)            
-		    if str(main_Url) != str(sec_url):
-		        #final_url = sec_url
-		        #else:
-			print sec_url
-			print main_Url
-			main_Url = sec_url
-			
-		    if main_Url not in seen_list and main_Url not in unseen_list:
-		        unseen_list.append(main_Url)
-		
-    
+            is_url_valid = validate_url(fullurl, seen_list, unseen_list)
+                                    
+            if is_url_valid and ':' not in new_link:
+                #Check for canonicals 
+		#print fullurl
+                final_url = check_canonical(fullurl)
+		            
+                if str(fullurl) != str(final_url):
+                    fullurl = final_url
+                    if fullurl not in seen_list and fullurl not in unseen_list:
+                        unseen_list.append(fullurl)
+                else:
+                    #No canonicals for this url
+                    unseen_list.append(fullurl)
+
+
     #Pop the next URL from queue to process.    
-    absolute_link = unseen_list.pop(0)
+    next_url_to_process = unseen_list.pop(0)
+    print "Next URL to process is :" + next_url_to_process
     
-    #Check and control the depth variables
+    #Check and adjust the depth variables
     depthCountdown = depthCountdown -1
     if depthCountdown == 0:
         crawl_depth = crawl_depth + 1
         increaseDepth = True
     
     #Debug print messages.
-    curren_seen_list_length = str(len(seen_list))
-    curren_unseen_list_length = str(len(list(unseen_list)))
-    
-    print "Seen List length is: "+curren_seen_list_length
-    print "Unseen List length is: " + curren_unseen_list_length
-    print depthCountdown
+    current_seen_list_length = str(len(seen_list))
+    current_unseen_list_length = str(len(list(unseen_list)))
+    current_new_list_length = str(len(list(new_list)))
+
+    print "Number of URL seen are: "+current_seen_list_length
+    print "Number of URL yet to be explored are: " + current_unseen_list_length
+
+    if keyphrase != "":
+        print "Number of links with keyphrase is: " + current_new_list_length
+
+    #This variable helps us to know how much time left for program to halt
+    if depthCountdown > 1:    
+        print "Count down to complete traversing current level is :  "+str(depthCountdown)
     
     #Recursive call to the function.
-    crawl(result_list,unseen_list, seen_list, crawl_depth, absolute_link, increaseDepth, depthCountdown)
+    crawl(result_list,unseen_list, seen_list, crawl_depth, next_url_to_process, increaseDepth, depthCountdown, new_list)
                     
 
-if __name__ == "__main__":
-    #Sanity Checks
-    if not(is_input_url_present):
-        print "Enter Seed URL to proceed"
-        print "Input Seed URL is: "+baseURL
-    #Call crawling algorithm
-    crawl(result_list,unseen_list,seen_list,start_depth,baseURL,False,1)
-    
-        
+#Call main crawling function only if seed url is there
+if len(sys.argv) >= 2:   
+    crawl(result_list,unseen_list,seen_list,start_depth,baseURL,False,1,new_list)
         
